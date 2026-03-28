488c32300e93fe951ee24b3187c730b0aad962b2
iaritina <iaritina7@gmail.com>
2026-03-28 22:01:26 +0300

    enhance documentation on AuthController

--- src/main/java/mg/tpws/restapi/controller/TicketController.java
+++ src/main/java/mg/tpws/restapi/controller/TicketController.java
@@ -9,7 +9,6 @@ import mg.tpws.restapi.service.TicketService;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
